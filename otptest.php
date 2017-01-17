1bf8060e4db66435099e3e3c1f2a817be0f09c23
akshit16 <jain.akshit16@gmail.com>
2017-01-17 22:39:21 +0530

    new files

---
+++ otptest.php
@@ -0,0 +1,56 @@
+<?php
+//Your authentication key
+$authKey = "137617Avrt117V587dfc16";
+
+//Multiple mobiles numbers separated by comma
+$mobileNumber = "919994329290";
+
+//Sender ID,While using route4 sender id should be 6 characters long.
+$senderId = "VERIFY";
+
+//Your message to send, Add URL encoding here.
+$message = urlencode("Test message");
+
+//Define route 
+$route = "4";
+//Prepare you post parameters
+$postData = array(
+    'authkey' => $authKey,
+    'mobiles' => $mobileNumber,
+    'message' => $message,
+    'sender' => $senderId,
+    'route' => $route
+);
+
+//API URL
+$url="https://control.msg91.com/api/sendhttp.php";
+
+// init the resource
+$ch = curl_init();
+curl_setopt_array($ch, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $postData
+    //,CURLOPT_FOLLOWLOCATION => true
+));
+
+
+//Ignore SSL certificate verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+
+//get response
+$output = curl_exec($ch);
+
+//Print error if any
+if(curl_errno($ch))
+{
+    echo 'error:' . curl_error($ch);
+}
+
+curl_close($ch);
+
+echo $output;
+?>
