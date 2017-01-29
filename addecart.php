003df9e279e9fe2cbe0a31c1851e1f4d220b7acc
sadhika2428 <sadhika.kataria@mindscroll.com>
2017-01-29 15:22:29 +0530

    insertion in ecart done

--- addecart.php
+++ addecart.php
@@ -1,3 +1,27 @@
 <?php
-echo $_REQUEST['id'];
+$id= $_REQUEST['id'];
+include_once("Connection.php");
+$result ="select * from additem where id  = '{$id}'";
+$res = mysqli_query($conn,$result);
+$row = mysqli_fetch_array($res);
+$row_cnt = mysqli_num_rows($res);
+echo $row['ProductName'];
+$sql="insert into ecart(reg,ProductName,Description,Price,Category,img_name,img_path,img_type) 
+values (
+'{$row["reg"]}',
+'{$row["ProductName"]}',
+'{$row["Description"]}',
+'{$row["Price"]}',
+'{$row["Category"]}',
+'{$row["img_name"]}',
+'{$row["img_path"]}',
+'{$row["img_type"]}'
+)";
+if(! mysqli_query( $conn, $sql ))
+{
+die(mysqli_error($conn));}
+else{ 
+echo "\n added to ecart successfully";
+}
+
 ?>
